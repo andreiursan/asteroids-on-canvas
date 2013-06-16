37a2f1ecc3727053f52f773271600cfae4f55eed
Andrei Ursan <hello@andreiursan.com>
2013-06-16 17:27:06 +0200

    integrated the animate library with the current Spaceship

--- js/spaceship.js
+++ js/spaceship.js
@@ -1,6 +1,7 @@
 function Spaceship(){
   this.x = canvas.width / 2;
   this.y = canvas.height / 2;
+  this.heading = 30;
 
   this.paint = function(context){
     context.fillStyle = "rgb(0,0,0)";
@@ -9,9 +10,10 @@ function Spaceship(){
     context.fillStyle = "rgb(255,0,0)";
     context.beginPath();
     context.translate(this.x, this.y);
-    context.moveTo(92, 110);
-    context.lineTo(100, 91)
-    context.lineTo(108, 110);
+    context.rotate(this.heading * Math.PI / 180.0);
+    context.moveTo(-8, 10);
+    context.lineTo(0, -9)
+    context.lineTo(8, 10);
     context.closePath();
     context.lineWidth = 2;
     context.fill();
