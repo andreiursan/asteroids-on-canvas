d5696782691b3a44c752a014cb0c9b790e5ecf09
Andrei Ursan <hello@andreiursan.com>
2013-06-18 19:46:33 +0200

    added a better look to the spaceship

--- js/spaceship.js
+++ js/spaceship.js
@@ -6,18 +6,11 @@ function Spaceship(){
   drift(this, this.heading, this.speed);
 
   this.paint = function(context){
-    context.strokeStyle = "rgb(255, 255, 255)";
-    context.fillStyle = "rgb(255,0,0)";
-    context.beginPath();
     context.translate(this.x, this.y);
     context.rotate(this.heading * Math.PI / 180.0);
-    context.moveTo(-8, 10);
-    context.lineTo(0, -9)
-    context.lineTo(8, 10);
-    context.closePath();
-    context.lineWidth = 2;
-    context.fill();
-    context.stroke(); 
+    var width = spaceshipSprite.width;
+    var height = spaceshipSprite.height;
+    context.drawImage(spaceshipSprite, -width/2, -height/2, width, height);
   }
 
   this.turnLeft = function() {
